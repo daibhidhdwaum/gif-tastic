9de0eadda8c2e14ceb53ec6d204ce713c9ec75f2
David Downie <daviddownie@gmail.com>
2018-10-28 13:39:49 -0400

    made image still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     
 var topic = ["Father John Misty", "The Smiths", "Radiohead", "Depeche Mode", "Arctic Monkeys", "Blur", "Lambchop"];
 var results;
-var APIKey = "gOhT5yVr007PciFEeeVBlG3xEWywuguj";
 
 //generates a button for each band in the array
 function createButton(){
@@ -14,19 +13,38 @@ function createButton(){
         bands.attr({
             "class": "btn",
             "data-band-name": topic[i]
-        });  
+        }); 
+        console.log(topic); 
     }
 }
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=&api_key=gOhT5yVr007PciFEeeVBlG3xEWywuguj";
+    createButton();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=gOhT5yVr007PciFEeeVBlG3xEWywuguj&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+        var results = response.data;
+        
+        for(var i = 0; i< results.length; i++) {
+
+            var gifDiv = $("<div>");
+
+            
+
+            var bandImage = $("<img>");
+            bandImage.attr("src", results[i].images.original_still.url);
+            console.log(results);
+
+            $(".gifs").prepend(gifDiv);
+        }
 
         console.log(queryURL);
         console.log(response);
+
+        gifDiv.prepend(bandImage);
+
+        
         });       
 
 });
